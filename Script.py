d57718c0652573aabdcf0e84fa8e0338d6226d25
Dadabhai02 <120179611+Dadabhai02@users.noreply.github.com>
2023-02-11 23:49:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -13,9 +13,8 @@ def is_enabled(value, default):
 
 class script(object):
     HOME_BUTTONURL_UPDATES = environ.get("HOME_BUTTONURL_UPDATES", 'https://t.me/MsFilmFactory_Bot')
-    START_TXT = environ.get("START_TXT", '''{},
-<b>Vaadi Nee Varuvanu Enaku Theriyum 😋 Apodiye Join Panitu Pooo 👀</b> <a href=https://t.me/{}>{}</a>,\n\n<b>I Can Provide MOVIES/SERIES And Lot More... Don't Waste Your Time by Looking To Add Me To Your Group & Enjoy Meh Power🤌🏻😎 </b>''')
-    HELP_TXT = """<b>Heyy Vro </b> {},<b>Join 👀</b> <a href=https://t.me/DaDa_Linkz></b><b>DaDa Linkz</b></a>"""
+    START_TXT = environ.get("START_TXT", '''Hey Vro{},<b>My Name is</b> <a href=https://t.me/{}>{}</a>,\n\n<b>I Can Provide MOVIES/SERIES And Lot More... Don't Waste Your Time by Looking To Add Me To Your Group & Enjoy Meh Power🤌🏻😎 </b>''')
+    HELP_TXT = """<b> {},<b>Vaadi Nee Varuvanu Enaku Theriyum 😋 Apodiye Join Panitu Pooo 👀</b> <a href=https://t.me/DaDa_Linkz></b><b>DaDa Linkz</b></a>"""
     ABOUT_TXT = """
 ╭────[ <b>About Me 🤧</b> ]────⍟
 │

3afe76b7983a2ced299bfb7802c40c82a63d60e9
Dadabhai02 <120179611+Dadabhai02@users.noreply.github.com>
2023-02-12 00:06:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -463,9 +463,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('', callback_data='')
         ], [
             InlineKeyboardButton('', callback_data=''),
-            InlineKeyboardButton('🏠 H𝙾𝙼𝙴 ', callback_data='start'),
+            InlineKeyboardButton('Back 🤌🏻 ', callback_data='start'),
         ], [
-            InlineKeyboardButton('Developer 🫣', url='https://t.me/DaDaXBhai'),
+            InlineKeyboardButton('Developer 🌼', url='https://t.me/DaDaXBhai'),
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
@@ -484,8 +484,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons = [[
-            InlineKeyboardButton('Developer 🫣', url='https://t.me/DaDaXBhai'),
-            InlineKeyboardButton('🏠 H𝙾𝙼𝙴 ', callback_data='start'),
+            InlineKeyboardButton('Developer 🌼', url='https://t.me/DaDaXBhai'),
+            InlineKeyboardButton('Back 🤌🏻 ', callback_data='start'),
          ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(

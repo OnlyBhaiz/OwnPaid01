b762cfffc7035ca5159690dc223977d3f80ba36c
Dadabhai02 <120179611+Dadabhai02@users.noreply.github.com>
2023-02-12 00:35:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -108,7 +108,7 @@ async def next_page(bot, query):
 
     btn.insert(0,
         [
-            InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://t.me/Tn68_tutorials/3')
+            InlineKeyboardButton(text="Hᴏᴡ Tᴏ Dᴏᴡɴʟᴏᴀᴅ⚡", url='https://t.me/DaDaXBhai/11')
         ]
     )
 

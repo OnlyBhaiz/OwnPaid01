1755d97650335878fad83d9b0577b9cff06d5561
Dadabhai02 <120179611+Dadabhai02@users.noreply.github.com>
2023-02-12 12:56:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -40,7 +40,7 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Haashim:Haashim@mfile0
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
-BASE_URL_OF_BOT = ('https://dadabhai.onrender.com')
+BASE_URL = ('https://dadabhai.onrender.com')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001616832136'))

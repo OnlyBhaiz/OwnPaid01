7f31598364fe6a368ecfb904c6557f572de117b3
OnlyBhaiz <126173308+OnlyBhaiz@users.noreply.github.com>
2023-02-25 12:02:23 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -11,4 +11,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("BOT IS RUNNING -Telegram@MrMalik")
+    return web.json_response("BOT IS RUNNING -Telegram@DaDa_Offil")

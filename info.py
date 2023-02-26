83c867c1a49b6c46d3daec9fe1bcd56e8a13dd21
OnlyBhaiz <126173308+OnlyBhaiz@users.noreply.github.com>
2023-02-26 18:28:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ PORT = environ.get("PORT", "8080")
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/7e1e3599d4c6f35342294.jpg https://telegra.ph/file/0053dea854ddf8c70028b.jpg https://telegra.ph/file/146fa3677b70828b74282.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/af795fa0562543d53b5cf.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1046893783 2131967391').split()]
